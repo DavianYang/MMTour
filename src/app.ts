c74f213278d7fd565889f8547e922ff0e465dc5e
DavianYang <kothantyarzarhein@gmail.com>
2021-06-24 22:23:05 +0630

    refactor: js to ts

---
+++ src/app.ts
@@ -0,0 +1,98 @@
+process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
+
+import path from "path";
+import express from "express";
+import cors from 'cors';
+import morgan from "morgan";
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
+import mongoSanitize from 'express-mongo-sanitize';
+import mongoose, { mongo } from "mongoose";
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+import { logger, stream } from "@utils/logger";
+
+
+class App{
+  public app: express.Application;
+  public port: string | number;
+  public env: string;
+
+  constructor(){
+    this.app = express()
+    this.port = process.env.PORT || 3000;
+    this.env = process.env.NODE_ENV || 'development';
+
+    this.initializeMiddlewares()
+    this.initializeSwagger()
+  }
+
+  public listen(){
+    this.app.listen(this.port, () => {
+      logger.info(`=================================`);
+      logger.info(`======= ENV: ${this.env} =======`);
+      logger.info(`🚀 App listening on the port ${this.port}`);
+      logger.info(`=================================`);
+    })
+  }
+
+  public getServer(){
+    return this.app
+  }
+
+  // private connectToDatabase(){
+  //   if (this.env !== 'production'){
+  //     mongoose.set('debug', true)
+  //   }
+    
+  //   mongoose.connect(DB, {
+  //     useNewUrlParser: true,
+  //     useCreateIndex: true,
+  //     useFindAndModify: false,
+  //     useUnifiedTopology: true
+  //   }).then(() => logger.info('DB connected successfully'));
+  // }
+
+  public initializeMiddlewares(){
+    const limiter = rateLimit({
+      max: 70, // limit each ip to 100 request per windowMS
+    })
+
+    if (this.env === 'production'){
+      this.app.use(morgan("combined", { stream }))
+      this.app.use(cors({ origin:  'your.domain.com', credentials: true}))
+      this.app.use('/api', limiter)
+    } else {
+      this.app.use(morgan('dev', { stream }));
+      this.app.use()
+    }
+    
+
+    this.app.use(hpp())
+    this.app.use(helmet())
+    this.app.use(express.json({ limit: '10kb' }))
+    this.app.use(express.urlencoded({ extended: true, limit: "10kb" }))
+    this.app.use(cookieParser())
+  }
+
+  private initializeSwagger() {
+    const options = {
+      swaggerDefinition: {
+        info: {
+          title: 'MMTour REST API',
+          version: '1.0.0',
+          description: 'Example doc',
+        }
+      },
+      // apis: ['swagger.yaml']
+    }
+
+    const specs = swaggerJSDoc(options);
+    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+  }
+}
+
+export default App;
