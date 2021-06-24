3f65ac8400c6718447919619a7ddbbaac3953727
DavianYang <kothantyarzarhein@gmail.com>
2021-06-24 23:02:03 +0630

    init

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,8 +5,8 @@ import winstonDaily from 'winston-daily-rotate-file';
 // logs dir
 const logDir = __dirname + '/../logs';
 
-if (!fs.existsSync(logDir)){
-    fs.mkdirSync(logDir)
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
 }
 
 // Define log format
@@ -17,43 +17,40 @@ const logFormat = winston.format.printf(({ timestamp, level, message }) => `${ti
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        logFormat
-    ),
-    transports: [
-       // debug log setting
-       new winstonDaily({
-           level: 'debug',
-           datePattern: 'YYYY-MM-DD',
-           dirname: logDir + '/debug',
-           maxFiles: 30,
-           json: false,
-           zippedArchive: true
-       }),
+  format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+  transports: [
+    // debug log setting
+    new winstonDaily({
+      level: 'debug',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/debug',
+      maxFiles: 30,
+      json: false,
+      zippedArchive: true,
+    }),
 
-       // error log settings
-       new winstonDaily({
-           level: 'error',
-           datePattern: 'YYYY-MM-DD',
-           dirname: logDir + '/debug',
-           maxFiles: 30,
-           json: false,
-           zippedArchive: true
-       })
-    ]
-})
+    // error log settings
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/debug',
+      maxFiles: 30,
+      json: false,
+      zippedArchive: true,
+    }),
+  ],
+});
 
 logger.add(
-    new winston.transports.Console({
-        format: winston.format.combine(winston.format.splat(), winston.format.colorize())
-    })
-)
+  new winston.transports.Console({
+    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
+  }),
+);
 
 const stream = {
-    write: (message: string) => {
-        logger.info(message.substring(0, message.lastIndexOf('\n')));
-    },
+  write: (message: string) => {
+    logger.info(message.substring(0, message.lastIndexOf('\n')));
+  },
 };
 
-export { logger, stream }
+export { logger, stream };
