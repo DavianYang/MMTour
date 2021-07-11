1dfe3d52041d23c431ffbec3d48ea3061503161c
DavianYang <kothantyarzarhein@gmail.com>
2021-07-11 18:10:07 +0630

    add user description to swaggers and apiFeatures

--- src/utils/apiFeatures.ts
+++ src/utils/apiFeatures.ts
@@ -1,24 +1,56 @@
-// class APIFeatures {
-//   public query;
-//   public queryString;
+import { Query, Document } from 'mongoose';
 
-//   constructor(query: Function, queryString: string) {
-//     this.query = query;
-//     this.queryString = queryString;
-//   }
+class APIFeatures {
+  public query;
+  public queryString;
 
-//   filter() {
-//     const queryObj = { ...(this.queryString as string) } as object;
-//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-//     excludedFields.forEach(el => delete queryObj[el]);
+  constructor(query: Query<Document[], Document>, queryString: object) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
-//     let queryStr = JSON.stringify(queryObj);
-//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  public filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-//     this.query = this.query.find(JSON.parse(queryStr));
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-//     return this;
-//   }
-// }
+    this.query = this.query.find(JSON.parse(queryStr));
 
-// export default APIFeatures;
+    return this;
+  }
+
+  public sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString;
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  public limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  public paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+export { APIFeatures };
